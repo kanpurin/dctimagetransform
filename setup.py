ed3eceb16c8207261e8769476d476e3bb74a7f38
kanpurin <kanpurin2@gmail.com>
2021-01-17 06:06:21 +0900

    いろいろ修正

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dct_image_transform",
+    name="dctimagetransform",
     version='1.0.0',
     description='DCT係数上での高速な画像変換',
     author='Kanchi Hibi',
     author_email='kanpurin2@gmail.com',
     install_requires=['numpy'],
     url='https://github.com/kanpurin/dctimagetransform',
-    packages=find_packages(),
-    test_suite='tests'
+    packages=find_packages("dct_image_transform"),
+    package_dir={"": "dct_image_transform"},
+    test_suite='tests',
+    include_package_data=True,
+    license='MIT'
 )
