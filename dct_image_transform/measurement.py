2f02845dddbe5823da097cb1a88d9a8e138f0ff0
kanpurin <kanpurin2@gmail.com>
2021-01-31 00:55:09 +0900

    measurement.pyにloop回数変更機能追加

--- dct_image_transform/measurement.py
+++ dct_image_transform/measurement.py
@@ -4,14 +4,17 @@ from skimage import io
 from dct import dct2
 from mask import mask
 import time
+import sys
 
 if __name__ == '__main__':
+    loop = 1000
+    if len(sys.argv) >= 2:
+        loop = int(sys.argv[1]) # 回数
     df = pd.DataFrame()
     np.set_printoptions(linewidth=200, precision=3, suppress=True)
     image = np.arange(1024*1024).reshape((1024,1024))
     print(image.shape)
     image = dct2(image)
-    loop = 1
     for j in range(1,11):
         for i in range(image.shape[0]//8//10*(j-1)+1,image.shape[0]//8//10*j,j):
             avg = 0
